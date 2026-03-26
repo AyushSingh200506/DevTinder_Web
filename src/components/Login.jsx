1763e89f61e1a27c9ff81ddadf7ada809d368d91
AyushSingh200506 <ayushsingh200506@gmail.com>
2026-03-26 12:37:49 +0530

    feat: restructure components and implement Redux for user management
    - Added Redux Toolkit for state management with user slice.
    - Created appStore for Redux store configuration.
    - Refactored components to use new directory structure under 'components'.
    - Implemented NavBar and Footer components.
    - Replaced old Login component with a new one that integrates with Redux and Axios for API calls.
    - Removed unused NavBar and Login components.
    - Added Profile and Feed components.
    - Updated App component to include Redux Provider and new component structure.
    - Introduced constants file for API base URL.

---
+++ src/components/Login.jsx
@@ -0,0 +1,56 @@
+import axios from "axios";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/UserSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+
+function Login() {
+  const [emailID, setEmailId] = useState("satya@gmail.com");
+  const [password, setPassword] = useState("Satya@123");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try{
+        const res = await axios.post(BASE_URL + "/login", {
+          emailID,
+          password
+        }, {
+          withCredentials: true
+        });
+        console.log("Login successful:", res.data);
+        dispatch(addUser(res.data));
+        return navigate("/");
+    } catch (err){
+        console.error("Login failed:", err);
+    }
+  }
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card card-border bg-base-300 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <fieldset className="fieldset mb-4">
+              <legend className="fieldset-legend">Email ID:</legend>
+              <input type="text" value={emailID} className="input" onChange={(e) => setEmailId(e.target.value)} />
+            </fieldset>
+             <fieldset className="fieldset mb-4">
+              <legend className="fieldset-legend">Password:</legend>
+              <input type="password" value={password} className="input" onChange={(e) => setPassword(e.target.value)} />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
